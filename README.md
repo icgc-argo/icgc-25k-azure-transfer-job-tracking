f98e1fd2de4064e57a5400945e32bfde51f7db79
Junjun Zhang <junjun.ca@gmail.com>
2021-07-06 09:12:09 -0400

    added job gen script, second batch of CLLE-ES jobs

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # Repository to track ICGC 25K Azure data transfer jobs
 
+Data transfer job organization:
+  1. all jobs are under `jobs` folder
+  2. jobs are divided by studies, use study_id as subfolder name under `jobs` folder
+  3. each study can further divide into batches, a batch is name by date in the format of 'YYYY-MM-DD'
+  4. each batch has a `batch-summary.tsv` file that lists all SONG analysis IDs and their assignment
+     to job_id. SONG analysis ID must be unique across all batches to avoid duplicate transfer
+
+Configuration settings for job scheduler:
+```
+  job_tracking_repo: git@github.com:icgc-argo/icgc-25k-azure-transfer-job-tracking.git
+  tracking_repo_push_interval: 12  # in hours
+  compute_environments:
+      rdpc_prod:
+        wes_url: xxxxx
+        max_parallel_runs: 2
+      rdpc_qa:
+        wes_url: xxxxx
+        max_parallel_runs: 4
+  scheduling_interval: 30  # in minutes
+```
+
+Job scheduler command line options:
+  studies: []  # list of study IDs
+
+Job scheduler procedure (run by cron) for each of the compute environments (update to the `scheduler` branch only):
+  0. bring in latest backlog jobs:
+    `git checkout main && git pull && git checkout scheduler && git merge main`
+  1. check statuses of every queued jobs from WES endpoind
+  2. if corresponding run is completed, verify all analysis objects are in PUBLISHED state at Azure,
+     then move the job to `completed` folder
+  3. if corresponding run is failed, move job to `failed` folder
+  4. if all jobs started within the most recent two scheduling windows are failed, likely something
+     wrong with the compute environment, send alert and stop schduling cycle
+  5. get job count still in `queued` folder, stop job scheduling if it's greater than or
+     equals to `max_parallel_runs`
+  6. pickup a new job from `backlog` folder, start it (or resume it if it was failed previously) and
+     then move the job to `queued` folder
+  7. repeat step 6 until `queued` job count equals `max_parallel_runs`
+
+Job tracking repo push procedure from scheduler:
+  1. determine when was the last push, stop if the interval is less than `tracking_repo_push_interval`
+  2. `git checkout main && git pull && git checkout scheduler && git merge main`
+  3. `git add jobs && git commit -m '[scheduler] update job status' && git push`
+
+Within 'jobs' direcotry, do this to report job status periodically:
+```
+for s in *-*;
+do
+  for f in `echo backlog queued failed completed`;
+    do echo -n "$s $f "; ls $s/*/$f/*/params.json |wc -l |sed 's/ //g';
+  done;
+done
+```
+
+Example output, which can be sent to a Slack channel:
+```
+PACA-CA backlog 1
+PACA-CA queued 2
+PACA-CA failed 1
+PACA-CA completed 1
+```
+
+Tracking repo updates by admins (update to the `main` branch only):
+  1. NEVER touch the `queued` directory which is used only by the `scheduler`
+  2. before any update, update to the latest `scheduler` branch and merge into `main`:
+     `git checkout scheduler && git pull && git checkout main && git merge scheduler`
+  3. do any updates as necessary, eg, adding more jobs; move failed jobs to backlog for resume; then push
---
+++ scripts/gen-transfer-jobs.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+import json
+import gzip
+import string
+import argparse
+from glob import glob
+from datetime import date
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+JOB_DIR = os.path.join(BASE_DIR, 'jobs')
+PAYLOAD_DIR = os.path.join(BASE_DIR, 'collab-song-payload-dumps')
+
+JOB_PARAMS = {
+    "_batch_id": None,
+    "_job_id": None,
+    "study_id": None,
+    "analysis_id": [],
+    "max_retries": 3,
+    "first_retry_wait_time": 60,
+    "cpus": 1,
+    "mem": 1,
+    "local_dir": "/icgc-argo-scratch",
+    "download_song_url": "https://song.cancercollaboratory.org",
+    "download_score_url": "https://storage.cancercollaboratory.org",
+    "download_cpus": 1,
+    "download_mem": 2,
+    "download_transport_mem": 2,
+    "upload_song_url": "https://song.azure-dev.overture.bio",
+    "upload_score_url": "https://score.azure-dev.overture.bio",
+    "upload_cpus": 1,
+    "upload_mem": 2,
+    "upload_transport_mem": 2
+}
+
+
+def get_new_batch_id(batch_ids=list()):
+    today = date.today().strftime('%Y-%m-%d')
+    if not batch_ids:
+        return today
+
+    last_batch_id = batch_ids[-1]
+    if last_batch_id == today:
+        return f"{today}a"
+
+    elif last_batch_id.startswith(today):
+        suffix = last_batch_id.replace(today, '')
+        if len(suffix) != 1 or suffix not in string.ascii_lowercase:
+            sys.exit(f"Last batch ID not valid: {last_batch_id}. Batch ID must follow pattern: 'YYYY-MM-DD[a-z]{0,1}'")
+        elif suffix == 'z':
+            sys.exit("Can no longer generate new batch of jobs for today.")
+
+        idx = string.ascii_lowercase.index(suffix)
+        return f"{today}{string.ascii_lowercase[idx + 1]}"
+
+    else:
+        return today
+
+
+def get_data_size(analysis):
+    data_size = 0
+    for f in analysis['file']:
+        data_size += f['fileSize']
+
+    return data_size
+
+
+def create_new_job(study, analysis_ids):
+    total_data_size = 0
+    for a in analysis_ids:
+        total_data_size += analysis_ids[a]
+
+    down_up_cpus = 1
+    if len(analysis_ids) == 1:  # single analysis job
+        down_up_cpus = int(total_data_size / 5000000000)  # 5GB per CPU
+
+    if down_up_cpus < 1:
+        down_up_cpus = 1
+
+    if down_up_cpus > 19:
+        down_up_cpus = 19
+
+    new_job = JOB_PARAMS.copy()
+    new_job.update({
+        'study_id': study,
+        'analysis_id': sorted(list(analysis_ids.keys())),
+        'download_cpus': down_up_cpus,
+        'download_mem': down_up_cpus * 2,  # 2 GB mem per cpu
+        'upload_cpus': down_up_cpus,
+        'upload_mem': down_up_cpus * 2,  # 2 GB mem per cpu
+        '_analysis_ids': analysis_ids,
+        '_total_data_size': total_data_size
+    })
+
+    return new_job
+
+
+def main(study, job_count, config):
+    # locate the payload dump in `PAYLOAD_DIR`
+    payload_dumps = sorted(glob(os.path.join(PAYLOAD_DIR, f"{study}.payloads.*.json.gz")))
+    if not payload_dumps:  # no dump found
+        sys.exit(f"Error: no SONG payload dump found under {PAYLOAD_DIR} for study {study}")
+    else:
+        payload_dump = payload_dumps[-1]  # use the latest one (if multiple exist)
+
+    with gzip.open(payload_dump, 'r') as p:
+        collab_song_analysis_objects = json.load(p)
+
+    # get all previous batches for the specified study, collect analysis IDs that
+    # have already been part of jobs previously generated
+    analysis_ids_with_job_generated = []
+    batch_summary_files = sorted(glob(os.path.join(JOB_DIR, study, '*', 'batch-summary.tsv')))
+
+    batch_ids = []
+    for f in batch_summary_files:
+        batch_ids.append(os.path.basename(os.path.dirname(f)))
+        with open(f, 'r') as s:
+            for line in s:
+                if line.startswith('#'):
+                    continue
+                analysis_ids_with_job_generated.append(line.split('\t')[3])
+
+    # assign new batch ID
+    new_batch_id = get_new_batch_id(batch_ids)
+
+    # prepare transfer jobs
+    new_jobs = []
+    analysis_ids = dict()
+    for analysis in collab_song_analysis_objects:
+        if analysis['study'] != study:
+            sys.exit(f"Error: different study '{analysis['study']}' in payload dump, specified study in command line: {study}")
+        if analysis['analysisId'] in analysis_ids_with_job_generated:
+            continue
+        if analysis['analysisState'] != 'PUBLISHED':
+            continue
+
+        data_size = get_data_size(analysis)
+        if data_size > config['max_analysis_data_size_to_be_pooled']:
+            new_jobs.append(create_new_job(study, {analysis['analysisId']: data_size}))
+            if len(new_jobs) == job_count:
+                break
+        else:
+            if len(analysis_ids) >= config['max_analyses_per_job']:
+                new_jobs.append(create_new_job(study, analysis_ids))
+                analysis_ids = dict()
+                if len(new_jobs) == job_count:
+                    break
+
+            analysis_ids[analysis['analysisId']] = data_size
+
+    if analysis_ids and len(new_jobs) < job_count:
+        new_jobs.append(create_new_job(study, analysis_ids))
+
+    # print(json.dumps(new_jobs, indent=2))
+
+    if not new_jobs:
+        print(f'Nothing to do. All analysis objects from {payload_dump} have been included in previously generated jobs.')
+        sys.exit()
+
+    # create batch directories
+    new_batch_dir = os.path.join(JOB_DIR, study, new_batch_id)
+    dirs = ['backlog', 'completed', 'failed', 'queued']
+    for d in dirs:
+        fullpath = os.path.join(new_batch_dir, d)
+        os.makedirs(fullpath)
+        open(os.path.join(fullpath, '.gitkeep'), 'a').close()
+
+    # generate batch-summary.tsv
+    batch_summary_f = open(os.path.join(new_batch_dir, 'batch-summary.tsv'), 'w')
+    i = 0
+    for job in new_jobs:
+        i += 1
+        job_id = 'job.' + (4 - len(str(i))) * '0' + str(i)
+
+        job['_batch_id'] = new_batch_id
+        job['_job_id'] = job_id
+        total_data_size = job.pop('_total_data_size')
+        analysis_ids = job.pop('_analysis_ids')
+        for a in analysis_ids:
+            fields = [job_id, str(len(analysis_ids)), str(total_data_size), a, str(analysis_ids[a])]
+            batch_summary_f.write("\t".join(fields) + "\n")
+
+        job_dir = os.path.join(new_batch_dir, 'backlog', job_id)
+        os.makedirs(job_dir)
+        with open(os.path.join(job_dir, 'params.json'), 'w') as j:
+            j.write(json.dumps(job, indent=2))
+
+    batch_summary_f.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ICGC 25k data Azure transfer job generator')
+    parser.add_argument('-s', '--study', type=str,
+                        help='SONG study', required=True)
+    parser.add_argument('-n', '--job-count', type=int,
+                        help='Number of jobs to be generated', required=True)
+    args = parser.parse_args()
+
+    config_file = os.path.join(BASE_DIR, 'scripts', 'job-gen.conf')
+    with open(config_file) as f:
+        config = yaml.safe_load(f)
+
+    if args.job_count > 9999:
+        sys.exit(f"Job count can not be greater than 9999: {args.job_count} specified")
+
+    if args.job_count > config['max_jobs_per_batch']:
+        sys.exit(f"According to the supplied config, job count per batch can not be "
+                 f"greater than {config['max_jobs_per_batch']}: {args.job_count} specified")
+
+    main(args.study, args.job_count, config)
