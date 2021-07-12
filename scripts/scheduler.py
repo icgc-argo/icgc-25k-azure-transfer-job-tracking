e7f068c6094fd426014dd22254c0388d865492ab
Junjun Zhang <junjun.ca@gmail.com>
2021-07-12 10:49:23 -0400

    minor format improvement for status summary

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -108,12 +108,12 @@ def get_job_status_summary():
     stdout, stderr, rc = run_cmd(cmd)
     job_status_changes = stdout.split('\n') if stdout else []
 
-    report_summary_lines = job_status_changes
+    report_summary_lines = job_status_changes + ['\n']  # add a new line separator
     changed_studies = set([])
     for change in job_status_changes:
         changed_studies.add(change.split(' ')[0])
 
-    for s in changed_studies:
+    for s in sorted(changed_studies):
         cmd = "ls -d jobs/%s/*/*/job.* |awk -F'/' '{print $2\" \"$4}' |sort |uniq -c |awk '{print $2\" \"$3\" \"$1}'" % s
         stdout, stderr, rc = run_cmd(cmd)
         report_summary_lines += stdout.split('\n') if stdout else []
